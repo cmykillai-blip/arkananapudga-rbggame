d049c6d09b4f3708fef7c0136689a2f9d5eec8e8
cmykillai-blip <cmykillai@gmail.com>
2025-12-04 13:01:26 +0200

    make Character script

---
+++ game/game/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace RPG_game
+
+{
+
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Hello, World!");
+
+        }
+
+    }
+
+}
+
