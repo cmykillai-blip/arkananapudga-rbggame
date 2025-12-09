ecaba70895264a6372eb3de4897eb8ecafeff776
cmykillai-blip <cmykillai@gmail.com>
2025-12-09 11:41:26 +0200

    Add Item Class

---
+++ ппр/ппр/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ппр
+{
+ 
+                public class Priest : IHealthable
+        {
+            public void FullHeal()
+            {
+                Console.WriteLine("FullHeal");
+            }
+
+            public void Heal()
+            {
+                Console.WriteLine("Heal");
+            }
+        }
+
+        public interface IHealthable
+        {
+            public void Heal();
+            public void FullHeal();
+        }
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                var newPriest = new Priest();
+                newPriest.FullHeal();
+                newPriest.Heal();
+            }
+        }
+    }
